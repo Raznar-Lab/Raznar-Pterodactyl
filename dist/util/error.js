620bbb8c413a325fc93a3f37661ee9c8f3a37cbd
xAbhista <53965703+xAbhista@users.noreply.github.com>
2021-05-27 08:33:57 +0700

    Removed dist dir

--- dist/util/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class DactylError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "RaznarError";
-    }
-}
-exports.default = DactylError;
