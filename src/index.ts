43b7f8462b0ceef66c920f88d0e77953d19db7ac
Yusta <53965703+NotYusta@users.noreply.github.com>
2021-08-03 15:50:25 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import Admin from "./admin";
 import http from "./util/http";
 
 export default class Pterodactyl {
-    constructor(public hostURL: string, private apiKey: string) {}
+    constructor(private hostURL: string, private apiKey: string) {}
     private clientRequest = (apiEndpoint: string, options?: RequestInit, contentType = "application/json"): Promise<Response> => http(this.hostURL, true, this.apiKey, apiEndpoint, options, contentType);
     private adminRequest = (apiEndpoint: string, options?: RequestInit, contentType = "application/json"): Promise<Response> => http(this.hostURL, false, this.apiKey, apiEndpoint, options, contentType);
 
