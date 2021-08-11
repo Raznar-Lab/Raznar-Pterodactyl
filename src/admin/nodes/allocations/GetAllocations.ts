fb70412e613aec97cdd9997b211d2a70873a0097
jeffryafandi <66934509+jeffryafandi@users.noreply.github.com>
2021-08-11 19:01:00 +0700

    Update GetAllocations.ts

--- src/admin/nodes/allocations/GetAllocations.ts
+++ src/admin/nodes/allocations/GetAllocations.ts
@@ -3,7 +3,7 @@ import DactylError from "../../../util/error";
 
 export default async function GetAllocations(request: IRequest, nodeID: number): Promise<IAdminAllocation> {
     try {
-        const response = await request(`/nodes/${nodeID}/allocations?per_page=Infinity`, {
+        const response = await request(`/nodes/${nodeID}/allocations?per_page=90000`, {
             method: "GET"
         });
         const json = await response.json();
